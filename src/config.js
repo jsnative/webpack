b5c3cfe3d43413c1365c26072d0179117316020c
Tolu Oluwagbemi <tulburg2@gmail.com>
2019-12-05 18:21:42 +0100

    update lib version

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 import Home from "@src/home";
+import Theme from "@js-native/lib/includes/theme";
 
 export default {
+  theme: Theme,
   routes: [
     { path: '/', component: Home, name: "Home" }
   ]
--- src/home.js
+++ src/home.js
@@ -1,14 +1,28 @@
-import { PageComponent } from '@core/components';
+import {
+  PageComponent, H1, P, Container
+} from '@core/components';
 
 export default class Home extends PageComponent {
 
-  state = {
-    title: "JS-Native App",
-    description: "Welcome to JS-Native"
-  }
   constructor() {
     // initialization
-    console.log("Hello world");
+    super();
+    const theme = defaultTheme;
+    this.backgroundColor(theme.background)
+      .padding(theme.padding.medium)
+      .color(theme.color);
+
+    const body = new Container();
+    body.addChild(
+      new H1().text("Welcome to JS-Native")
+        .textAlign('center')
+        .fontFamily('helvetica, sans-serif')
+        .lineHeight('1.6'),
+      new P().text("Welcome to the new world of Pure javascript components")
+        .textAlign('center')
+        .fontSize(theme.small)
+    );
+    this.addChild(body);
   }
 }
 
